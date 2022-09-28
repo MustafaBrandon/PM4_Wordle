81ad45cbf6f1b8baa7c424281a1521713286581c
Brandon <brandonjmhunt@gmail.com>
2022-09-28 13:43:54 -0600

    feat: :sparkles: Parse Input + color examples

--- Wordle.py
+++ Wordle.py
@@ -12,19 +12,40 @@ from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
 
 def wordle():
 
+
+
+    def get_guess(row):
+        # currentRow = gw.get_current_row()
+        currentGuess = ''
+        for i in range(5):
+            currentGuess += gw.get_square_letter(row, i)
+        return currentGuess
+
     def enter_action(s):
-        gw.show_message("You have to implement this method.")
+
+        currentGuess = get_guess(0)
+        print(currentGuess)
+
+        gw.set_square_color(0,0,'green')
+        gw.set_square_color(0,1,'yellow')
+        gw.set_square_color(0,2,'grey')
+
+        # Increment current row, needs logic for end of game
+        gw.set_current_row(gw.get_current_row() + 1)
+
         # Check if user filled out five letters
         # Check if entered word is actual word (optional)
         # Trigger comparison logic of entered and wordToGuess
         # Update GUI and move on to next round or terminate game
 
+
     # On game start select a random word from our word list
     randIndex = random.randint(0, len(FIVE_LETTER_WORDS))
     wordToGuess = FIVE_LETTER_WORDS[randIndex]
-    # print(wordToGuess)
+    print(wordToGuess)
 
     gw = WordleGWindow()
+    gw.show_message("Enter your guess!")
     gw.add_enter_listener(enter_action)
 
 # Startup code
